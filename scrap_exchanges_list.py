de1091a94a1e8fd94c47857657564072de834b43
Esubalew <esubalew.a2009@gmail.com>
2024-07-19 08:04:51 +0300

    write companies slugs to csv file

--- scrap_exchanges_list.py
+++ scrap_exchanges_list.py
@@ -42,6 +42,38 @@ def scraped_exchanges_urls(soup):
             links.append(a_tag['href'])
     return links
     
+def write_to_csv(data, filename='results.csv'):
+    file_exists = os.path.isfile(filename)
+
+    # Read existing links to avoid duplicates
+    existing_links = set()
+    if file_exists:
+        with open(filename, mode='r', newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader, None)  # Skip header if file is not empty
+            existing_links = {row[0] for row in reader}  # Collect existing slugs
+
+    # Write new rows
+    with open(filename, mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+
+        # Write header only if the file did not exist
+        if not file_exists:
+            writer.writerow(['Slug', 'Name', 'Web', 'X (Twitter)', 'Founders'])
+
+        # Write new data, avoid duplicates
+        for url in data:
+            slug = url.replace('/exchanges/', '').strip('/')
+            if slug not in existing_links:
+                writer.writerow([
+                    slug, 
+                    '',  # Placeholder for 'Name'
+                    '',  # Placeholder for 'Web'
+                    '',  # Placeholder for 'X (Twitter)'
+                    ''   # Placeholder for 'Founders'
+                ])
+                existing_links.add(slug)
+
 driver = open_browser()
 
 exchanges_url = "https://coinmarketcap.com/rankings/exchanges/"
@@ -50,3 +82,6 @@ click_show_more(driver, 'button.sc-7d96a92a-0.hOXHRi')
 soup = BeautifulSoup(driver.page_source, "html.parser")
 
 scraped_exchanges_urls_data = scraped_exchanges_urls(soup)
+write_to_csv(scraped_exchanges_urls_data)
+
+close_driver(driver)
